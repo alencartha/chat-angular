9d3aa46a0b446c8ed5e4238291895a682a19e553
alencartha <alencar_tha@hotmail.com>
2021-07-06 10:09:03 -0300

    add firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
+
+const config = {
+  apiKey: 'AIzaSyBkw0KKsnIkDrFgfQw5u4B8qVCRTTpsKg4',
+  databaseURL: 'https://chat-angular-26d93-default-rtdb.firebaseio.com',
+};
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  constructor() {}
+
   title = 'chat-angular';
 }
