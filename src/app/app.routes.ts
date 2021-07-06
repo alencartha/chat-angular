58804aa9d61972e157613d379928e81f7b9bacce
Tânagra Andria <design.andria@gmail.com>
2021-07-06 15:06:11 -0300

    create routes, and components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,21 @@
-import { LoginComponent } from './login/login.component';
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
 
-export const ROUTES: Routes = [
+import { LoginComponent } from './login/login.component';
+import {ListComponent} from './list/list.component';
+import { NewRoomComponent } from './new-room/new-room.component';
+import { ChatComponent } from './chat/chat.component';
+
+const routes: Routes = [
   { path: '', component: LoginComponent },
-  { path: 'login', component: LoginComponent },
+  { path: 'list', component: ListComponent },
+  { path: 'newRoom', component: NewRoomComponent },
+  { path: 'chat', component: ChatComponent },
 ];
+
+@NgModule ({
+  imports:[RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule {}
